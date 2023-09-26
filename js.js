43227939a39ccdea318ed21372db68936fb04263
MLK27 <abdelmaleklatroch2022@gmail.com>
2023-09-26 20:10:02 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 //books - main div holding all the books
 const books = document.querySelector(".books");
 //add book button
-const addBook = document.querySelector(".add-book");
+const addBook = document.querySelector(".new");
 //add/edit book modal
 const modal = document.querySelector("#modal");
 //this is the x button to close a modal
@@ -134,7 +134,7 @@ function fillOutEditForm(book) {
 //create the edit icon w/ event listener
 function createEditIcon(book) {
   const editIcon = document.createElement("img");
-  editIcon.src = "../icons/pencil.svg";
+  editIcon.src = "/icons/pencil-svgrepo-com.svg";
   editIcon.setAttribute("class", "edit-icon");
   editIcon.addEventListener("click", () => {
     fillOutEditForm(book);
@@ -143,20 +143,7 @@ function createEditIcon(book) {
 }
 
 //create dummy icons, they don't do anything
-function createIcons() {
-  const div = createBookElement("div", "", "icons");
-  const icon1 = document.createElement("img");
-  icon1.src = "../icons/star-plus-outline.svg";
-  const icon2 = document.createElement("img");
-  icon2.src = "../icons/eye-plus-outline.svg";
-  const icon3 = document.createElement("img");
-  icon3.src = "../icons/source-branch.svg";
 
-  div.appendChild(icon1);
-  div.appendChild(icon2);
-  div.appendChild(icon3);
-  return div;
-}
 
 function deleteBook(index) {
   myLibrary.splice(index, 1);
@@ -170,19 +157,38 @@ function createBookItem(book, index) {
   bookItem.setAttribute("key", index);
   bookItem.setAttribute("class", "card book");
   bookItem.appendChild(
-    createBookElement("h1", `Title: ${book.title}`, "book-title")
+    createBookElement("h1", `${book.title}`, "book-title")
   );
   bookItem.appendChild(
-    createBookElement("h1", `Author: ${book.author}`, "book-author")
+    createBookElement("h1", `By: ${book.author}`, "book-author")
   );
   bookItem.appendChild(
-    createBookElement("h1", `Pages: ${book.pages}`, "book-pages")
+    createBookElement("h1", `Number of pages: ${book.pages}`, "book-pages")
   );
   bookItem.appendChild(createReadElement(bookItem, book));
   bookItem.appendChild(createBookElement("button", "X", "delete"));
-  bookItem.appendChild(createIcons());
   bookItem.appendChild(createEditIcon(book));
+  // Create the div for project icons
+  const projectIcons = document.createElement("div");
+  projectIcons.setAttribute("class", "project-icons");
+  
+  // Create and append the <i> elements for the icons
+  const starIcon = document.createElement("i");
+  starIcon.setAttribute("class", "fa-solid fa-star");
+  
+  const eyeIcon = document.createElement("i");
+  eyeIcon.setAttribute("class", "fa-solid fa-eye-slash");
+  
+  const shareIcon = document.createElement("i");
+  shareIcon.setAttribute("class", "fa-solid fa-share");
+  
+  projectIcons.appendChild(starIcon);
+  projectIcons.appendChild(eyeIcon);
+  projectIcons.appendChild(shareIcon);
 
+  bookItem.appendChild(projectIcons);
+
+  bookItem.appendChild(createEditIcon(book));
   bookItem.querySelector(".delete").addEventListener("click", () => {
     deleteBook(index);
   });
